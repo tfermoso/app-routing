aad1d63c03f89eec2e727974b8d0c05993feab9f
tfermoso <tomas.fermoso@gmail.com>
2017-04-15 21:16:44 +0200

    01. routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { MembersComponent } from './members/members.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    MembersComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path:'members',
+        component:MembersComponent
+      },
+       {
+        path:'products',
+        component:ProductsComponent
+      },
+      { path: '',   redirectTo: '/members', pathMatch: 'full' },
+      {
+        path:'**',
+        component:PagenotfoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
