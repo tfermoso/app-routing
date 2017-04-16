fb365ba148adf5345a20f3467c4c95f4f313f67c
tfermoso <tomas.fermoso@gmail.com>
2017-04-16 09:07:48 +0200

    0.5 Routing recibiendo parámetros

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'product',
-  template: `
+    selector: 'product',
+    template: `
     <h2>Product detail</h2>
     <p>Product id: {{id}} </p>
   `,
-  styleUrls: ['./products.component.css']
+    styleUrls: ['./products.component.css']
 })
 export class ProductComponent implements OnInit {
-    id:number=12;
-  constructor() { }
+    id: number = 12;
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {}
+    
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.id = +this.route.snapshot.params['id'];
+
+
+    }
 
 }
