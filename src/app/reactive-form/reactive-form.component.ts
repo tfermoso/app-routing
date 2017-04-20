775e743264c5a35a305246caa48fe2cccbcba4f6
tfermoso <tomas.fermoso@gmail.com>
2017-04-20 10:14:10 +0200

    Reactive Form 03 Usando FormBuilder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormBuilder, FormGroup ,FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,17 +8,27 @@ import { FormBuilder, FormGroup ,FormControl, Validators} from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+  userform:FormGroup;
+  constructor(private _formBuilder:FormBuilder){
+
+  }
+  /*
   userform=new FormGroup({
     name:new FormControl('Tomas',[Validators.required,Validators.minLength(4),Validators.maxLength(10)]),
     apellidos:new FormControl('',Validators.required),
     email:new FormControl('',Validators.email),
     postalCode:new FormControl('',Validators.pattern('^[1-9][0-9]{4}'))
   })
+  */
  
    
-
-
   ngOnInit() {
+    this.userform=this._formBuilder.group({
+      name: ['Tomas',[Validators.required,Validators.minLength(4),Validators.maxLength(10)]],
+      apellidos:['',Validators.required],
+      email:['',Validators.email],
+      postalCode:['',Validators.pattern('^[1-9][0-9]{4}')]
+    })
   }
   
   onSubmit():void{
